e1b58e79238e1f06c95c27a3dafe52ae54a3eb96
Evryatis <48336818+Evryatis@users.noreply.github.com>
2024-04-22 17:28:03 +0200

    mise a jour des constantes ajout des couleurs

--- TPSAE/src/main/java/vue/ConstantesCanva.java
+++ TPSAE/src/main/java/vue/ConstantesCanva.java
@@ -8,8 +8,17 @@ public interface ConstantesCanva {
     public final Color COULEUR_GRILLE = Color.BLACK;
     public final int CARRE = 40;
     public final Color COULEUR_APPRENTI = Color.BROWN;
+    public final Color COULEUR_BLEU = Color.BLUE;
+    public final Color COULEUR_ROUGE = Color.RED;
+    public final Color COULEUR_JAUNE = Color.YELLOW;
+    public final Color COULEUR_PURPLE = Color.PURPLE;
+    public final Color COULEUR_BLACK = Color.BLACK;
     public final Color COULEUR_POSITION = Color.BLUE;
     public final int LARGEUR_OVALE = 30;
     public final int HAUTEUR_OVALE = 30;
+
+
+
+
 
 }
