578b6b85550055e7feee5be6080db005f0a15f03
Evryatis <48336818+Evryatis@users.noreply.github.com>
2024-05-16 14:31:25 +0200

    Update Controleur.java

--- TPSAE/src/main/java/controleur/Controleur.java
+++ TPSAE/src/main/java/controleur/Controleur.java
@@ -12,15 +12,13 @@ import java.util.Collection;
 
 public class Controleur implements EventHandler {
 
-
-    @Override
-    public void handle(Event event){
+   public void handle(Event event){
         Object userData = ((MenuItem)event.getSource()).getUserData();
         if(userData instanceof File){//l'user vient de choisir un scenario
             File fichierScenario = (File) userData;
             System.out.println(fichierScenario.getName());
-            File scenario = fichierScenario;
             Collection <Temple> temples = LectureScenario.lecture(fichierScenario);
+
             VBoxRoot.getApprenti().setTemples(temples);
             System.out.println(VBoxRoot.getApprenti());
 
@@ -28,6 +26,7 @@ public class Controleur implements EventHandler {
             // effacer chaque carré
             // mettre ls temples & cristaux
             // good luck
+    }
     }
 }
 }
