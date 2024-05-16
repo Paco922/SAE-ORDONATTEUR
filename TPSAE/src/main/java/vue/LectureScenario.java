d656aca3f09fe0a64a1f7209784348288b682ae6
Evryatis <48336818+Evryatis@users.noreply.github.com>
2024-05-16 14:30:30 +0200

    Mise a jour de la structure de LectureScenario

--- TPSAE/src/main/java/vue/LectureScenario.java
+++ TPSAE/src/main/java/vue/LectureScenario.java
@@ -1,11 +1,11 @@
 package vue;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Scanner;
+import java.util.*;
 import java.io.File;
 
+
+import javafx.geometry.Pos;
 import modele.Position;
 import modele.Temple;
 import static vue.ConstantesCanva.*;
@@ -15,10 +15,15 @@ public class LectureScenario {
     /*
     La methode lecture prend en param un fichier contenant un scenario et retourne une collection de donnée
      */
+
+    // Les variables champs
+    private Position position;
+    private int couleur; // Assuming couleur is an int representing color
+    private int cristal; // Assuming cristal is an int
     public static Collection<Temple> lecture(File fichierScenario) {
 
-        Collection<Temple> templesDuScenario = new ArrayList<>();
         // Remplacer la liste par un type de collection plus adapté
+        Map<Position, Temple> templesDuScenario = new HashMap<>();
 
         try {
             Scanner scanner = new Scanner(fichierScenario);
@@ -29,21 +34,32 @@ public class LectureScenario {
                 int posX = scanner.nextInt() + LARGEUR_CANVA / (2 * CARRE);
                 // Pourquoi ajouter LARGEUR_CANVAS/(2*CARRE)?
 
-                int posY = scanner.nextInt() + HAUTEUR_CANVAS / (2 * CARRE);
+                int posY = scanner.nextInt() + HAUTEUR_CANVA / (2 * CARRE);
 
                 int couleur = scanner.nextInt();
                 int cristal = scanner.nextInt();
                 temple = new Temple(new Position(posX, posY), couleur, cristal);
-                templesDuScenario.add(temple);
+                templesDuScenario.put(temple.getPosition(), temple);
             }
+            System.out.println(templesDuScenario);
+
 
             scanner.close();
         }
        catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
        }
-       return templesDuScenario;
+       return (Collection<Temple>) templesDuScenario;
     }
+
+    public String toString() {
+        return "Temple{" +
+                "position=" + position +
+                ", couleur=" + couleur +
+                ", cristal=" + cristal +
+                '}';
+    }
+
 
 
 }
