ff1d101a5018cd283306fce94c58d2c43856cc17
Evryatis <48336818+Evryatis@users.noreply.github.com>
2024-05-17 22:19:47 +0200

    Ajout d'un toString à la classe LectureScenario, modification du stockage des temples en HashMap

--- TPSAE/src/main/java/vue/LectureScenario.java
+++ TPSAE/src/main/java/vue/LectureScenario.java
@@ -12,18 +12,14 @@ import static vue.ConstantesCanva.*;
 
 
 public class LectureScenario {
-    /*
-    La methode lecture prend en param un fichier contenant un scenario et retourne une collection de donnée
-     */
-
     // Les variables champs
     private Position position;
     private int couleur; // Assuming couleur is an int representing color
     private int cristal; // Assuming cristal is an int
-    public static Collection<Temple> lecture(File fichierScenario) {
 
+    public static HashMap<Position, Temple> lecture(File fichierScenario) {
         // Remplacer la liste par un type de collection plus adapté
-        Map<Position, Temple> templesDuScenario = new HashMap<>();
+        HashMap<Position, Temple> templesDuScenario = new HashMap<>();
 
         try {
             Scanner scanner = new Scanner(fichierScenario);
@@ -32,34 +28,28 @@ public class LectureScenario {
                 // LARGEUR_CANVAS = 31 HAUTEUR_CANVA = 31
                 // permet de traiter tout les scénarios proposés
                 int posX = scanner.nextInt() + LARGEUR_CANVA / (2 * CARRE);
-                // Pourquoi ajouter LARGEUR_CANVAS/(2*CARRE)?
-
                 int posY = scanner.nextInt() + HAUTEUR_CANVA / (2 * CARRE);
-
                 int couleur = scanner.nextInt();
                 int cristal = scanner.nextInt();
                 temple = new Temple(new Position(posX, posY), couleur, cristal);
                 templesDuScenario.put(temple.getPosition(), temple);
             }
             System.out.println(templesDuScenario);
-
-
             scanner.close();
-        }
-       catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
-       }
-       return (Collection<Temple>) templesDuScenario;
+        }
+        return templesDuScenario;
     }
 
+    @Override
     public String toString() {
-        return "Temple{" +
+        return "LectureScenario{" +
                 "position=" + position +
                 ", couleur=" + couleur +
                 ", cristal=" + cristal +
                 '}';
     }
-
-
-
 }
+
+
